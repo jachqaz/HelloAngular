89d8cd772c15c57ca841d91cb6c8ba0879c4c593
JOHAN <Jach_1997@hotmail.com>
2018-08-09 15:50:33 -0500

    directives

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -7,7 +7,7 @@ import {Component, OnInit} from '@angular/core';
 })
 export class CoursesComponent implements OnInit {
   title = 'List of course';
-
+  courses = ['course1', 'course2', 'course3'];
   constructor() {
   }
 
