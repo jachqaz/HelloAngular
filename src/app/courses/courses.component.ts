8cabcc7ec462a68836048b2b6cc35246239bcf6f
JOHAN <Jach_1997@hotmail.com>
2018-08-10 01:03:28 -0500

    Atribute Binding

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -10,6 +10,7 @@ export class CoursesComponent implements OnInit {
   title = 'List of course';
   courses;
   imageUrl = 'http://dragonball.sullca.com/wp-content/uploads/2015/07/Dragon-Ball-Super-online.jpg';
+  colSpan = 2;
 
   constructor(service: CoursesService) {
     this.courses = service.getCourses();
