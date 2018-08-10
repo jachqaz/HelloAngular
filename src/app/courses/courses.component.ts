20a3b3883a3c2cbbb0a7a18994dc947ee3ca90c2
JOHAN <Jach_1997@hotmail.com>
2018-08-10 14:45:40 -0500

    Pipes

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -7,6 +7,14 @@ import {CoursesService} from './courses.service';
   styleUrls: ['./courses.component.css']
 })
 export class CoursesComponent implements OnInit {
+
+  course = {
+    title: 'The Complete Angular Course',
+    rating: 4.9745,
+    students: 30123,
+    price: 190.95,
+    relaseDate: new Date(2016, 3, 1)
+  };
   title = 'List of course';
   courses;
   imageUrl = 'http://dragonball.sullca.com/wp-content/uploads/2015/07/Dragon-Ball-Super-online.jpg';
