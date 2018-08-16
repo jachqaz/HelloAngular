047b25e0efe726effa827dc8f2c8ae5373f500ca
JOHAN <Jach_1997@hotmail.com>
2018-08-16 14:25:51 -0500

    Input Properties

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import {CoursesComponent} from './courses/courses.component';
 import {CoursesService} from './courses/courses.service';
 import {FormsModule} from '@angular/forms';
 import {SummaryPipe} from './summary.pipe';
+import {FavoriteComponent} from './favorite/favorite.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CoursesComponent,
-    SummaryPipe
+    SummaryPipe,
+    FavoriteComponent
   ],
   imports: [
     BrowserModule,
